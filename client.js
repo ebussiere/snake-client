5d832576d74128704d30d3110c16ba8b3905758e
ebussiere <eddy@embtech.ca>
2020-09-23 21:14:35 +0000

    with moves

--- client.js
+++ client.js
@@ -1,4 +1,6 @@
 const net = require('net');
+
+
 const connect = function() {
   const conn = net.createConnection({
     host: '135.23.222.131',
@@ -8,11 +10,36 @@ const connect = function() {
   conn.setEncoding('utf8');
   conn.on('connect', () => {
     console.log('Connection Established');
-  });
 
+  });
   conn.on('connect', () => {
     conn.write('Name: JFK');
-  });
+    moves();
+  }
+  );
+
+  const moves = () => {
+    setTimeout(() => {
+      conn.write('Move: up');
+    }, 100);
+    setTimeout(() => {
+      conn.write('Move: up');
+    }, 200);
+    setTimeout(() => {
+      conn.write('Move: left');
+    }, 300);
+    setTimeout(() => {
+      conn.write('Move: left');
+    }, 400);
+    setTimeout(() => {
+      conn.write('Move: left');
+    }, 500);
+    setTimeout(() => {
+      conn.write('Move: left');
+    }, 600);
+  };
+
+
 
   conn.on('data', (data) => {
     console.log('Server says: ', data);
