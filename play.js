c3e3331a27cbd87043cd5c6da87a0708db88f687
ebussiere <eddy@embtech.ca>
2020-09-23 21:20:01 +0000

    setupinput

--- play.js
+++ play.js
@@ -4,3 +4,16 @@ const connect = require('./client');
 console.log('Connecting ...');
 connect();
 
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.on('data', (key) => {
+    if (key === '\u0003') {
+      process.exit();
+    }
+  });
+  stdin.resume();
+  return stdin;
+};
+
