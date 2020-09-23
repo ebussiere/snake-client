cc5fcbfea01ae1af49ad06e5e59a295e13d3dd3c
ebussiere <eddy@embtech.ca>
2020-09-23 21:48:23 +0000

    added stdin

--- play.js
+++ play.js
@@ -1,19 +1,7 @@
 //const net = require('net');
 const connect = require('./client');
+const { setupInput } = require('./input');
 
 console.log('Connecting ...');
 connect();
-
-const setupInput = function() {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding('utf8');
-  stdin.on('data', (key) => {
-    if (key === '\u0003') {
-      process.exit();
-    }
-  });
-  stdin.resume();
-  return stdin;
-};
-
+setupInput();
